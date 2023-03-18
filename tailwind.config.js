66f1aed3c7f43b9b6de9f878d7147c0676f1c333
DavidAshburn <oneinchwebbing@gmail.com>
2023-03-18 00:17:55 -0700

    google fonts working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./**/*.php"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        'abril': ['Abril Fatface', 'cursive'],
+        'garamond': ['Cormorant Garamond', 'serif'],
+        'ebgaramond': ['EB Garamond', 'serif'],
+        'inter': ['Inter Tight', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'nunito': ['Nunito', 'sans-serif'],
+        'playfair': ['Playfair Display', 'serif'],
+        'tinos': ['Tinos', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
