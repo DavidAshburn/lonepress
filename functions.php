bbc6540b6fa4d5a47062ac208661d1b729ee9d60
DavidAshburn <oneinchwebbing@gmail.com>
2023-03-18 23:12:18 -0700

    poets taxonomy created for poem custom post type

--- functions.php
+++ functions.php
@@ -13,42 +13,42 @@ function lone_enqueue_styles() {
 add_action('wp_enqueue_scripts', 'lone_enqueue_styles');
 
 //------------------------------------------------------------------------------------------------------------
-// Custom Post Types ( Poets, Poems )
+// Custom Post Types ( Poems )
 //------------------------------------------------------------------------------------------------------------
 
-function lone_create_poet_type() {
-	$poet_labels = array(
-		'name' => _X('Poets', 'Post Type General Name', 'lone'),
-		'singular_name' => _X('Poet', 'Post Type Singular Name', 'lone'),
-		'menu_name' => __('Poets', 'lone'),
-		'parent_item_colon' => __('Parent Poet', 'lone'),
-		'all_items' => __('All Poets', 'lone'),
-		'view_item' => __('View Poet', 'lone'),
-		'add_new_item' => __('Add Poet', 'lone'),
+function lone_create_poem_type() {
+	$poem_labels = array(
+		'name' => _X('Poems', 'Post Type General Name', 'lone'),
+		'singular_name' => _X('Poem', 'Post Type Singular Name', 'lone'),
+		'menu_name' => __('Poems', 'lone'),
+		'parent_item_colon' => __('Parent Poem', 'lone'),
+		'all_items' => __('All Poems', 'lone'),
+		'view_item' => __('View Poem', 'lone'),
+		'add_new_item' => __('Add Poem', 'lone'),
 		'add_new' => __('Add New', 'lone'),
-		'edit_item' => __('Edit Poet', 'lone'),
-		'update_item' => __('Update Poet', 'lone'),
-		'search_items' => __('Search Poets', 'lone'),
+		'edit_item' => __('Edit Poem', 'lone'),
+		'update_item' => __('Update Poem', 'lone'),
+		'search_items' => __('Search Poems', 'lone'),
 		'not_found' => __('Not Found', 'lone'),
 		'not_found_in_trash' => __('Not found in Trash', 'lone')
 	);
 
-	$poet_args = array(
-		'label' => __('poets', 'lone'),
-		'description' => __('LMLS Contributing Poets', 'lone'),
-		'labels' => $poet_labels,
+	$poem_args = array(
+		'label' => __('poems', 'lone'),
+		'description' => __('Poems', 'lone'),
+		'labels' => $poem_labels,
 
 		//Post Editor features
-		'supports' => array('title', 'excerpt', 'author', 'custom-fields'),
+		'supports' => array('title', 'editor', 'excerpt', 'author', 'custom-fields'),
 
 		//associating with taxonomy
 		// 'taxonomies' => 
 
 		'heirarchical' => true,
 		'public' => true,
+		'rewrite' => array('slug' => 'poems'),
 		'show_ui' => true,
 		'show_in_menu' => true,
-		'menu_position' => 1,
 		'can_export' => true,
 		'has_archive' => true,
 		'exclude_from_search' => false,
@@ -57,29 +57,43 @@ function lone_create_poet_type() {
 		'show_in_rest' => true
 	);
 
-	register_post_type( 'poets', $poet_args);
+	register_post_type( 'poems', $poem_args);
 
 }
 
-function lone_create_poem_type() {
-
-	register_post_type( 'poems', 
-		array(
-			'labels' => array(
-				'name' => __('Poems'),
-				'singular' => __('Poem')
-			),
-			'public' => true,
-			'has_archive' => true,
-			'rewrite' => array('slug' => 'poems'),
-			'show_in_rest' => true
-		)
-	);
-}
-
-add_action('init', 'lone_create_poet_type', 0);
-add_action('init', 'lone_create_poem_type');
+add_action('init', 'lone_create_poem_type', 0);
 
 //------------------------------------------------------------------------------------------------------------
+// Custom Taxonomies ( Poets )
+//------------------------------------------------------------------------------------------------------------
+
+function lone_create_poet_taxonomy() {
+
+	$labels = array(
+		'name' => _X( 'Poets', 'taxonomy general name' ),
+    	'singular_name' => _X( 'Poet', 'taxonomy singular name' ),
+    	'search_items' =>  __( 'Search Poets' ),
+    	'all_items' => __( 'All Poets' ),
+    	'parent_item' => __( 'Parent Poet' ),
+    	'parent_item_colon' => __( 'Parent Poet:' ),
+    	'edit_item' => __( 'Edit Poet' ), 
+    	'update_item' => __( 'Update Poet' ),
+    	'add_new_item' => __( 'Add New Poet' ),
+    	'new_item_name' => __( 'New Poet Name' ),
+    	'menu_name' => __( 'Poets' ),
+	);
+
+	register_taxonomy('poets', array('poems'), array(
+		'hierarchical' => true,
+    	'labels' => $labels,
+    	'show_ui' => true,
+    	'show_in_rest' => true,
+    	'show_admin_column' => true,
+    	'query_var' => true,
+	    'rewrite' => array( 'slug' => 'poet' ),
+	));
+}
+
+add_action( 'init', 'lone_create_poet_taxonomy', 0 );
 
 ?>
