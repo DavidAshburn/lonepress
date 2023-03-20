fde1368ca819e4267a537fd02a67bad68ab0896c
DavidAshburn <oneinchwebbing@gmail.com>
2023-03-20 16:44:26 -0700

    indentation

--- functions.php
+++ functions.php
@@ -50,27 +50,27 @@ add_action('wp_enqueue_scripts', 'lone_enqueue_styles');
 function lone_create_poet_taxonomy() {
 
 	$labels = array(
-		'name' 				=> _x( 'Poets', 'taxonomy general name' ),
-    	'singular_name' 	=> _x( 'Poet', 'taxonomy singular name' ),
-    	'search_items' 		=> __( 'Search Poets' ),
-    	'all_items' 		=> __( 'All Poets' ),
-    	'parent_item' 		=> __( 'Parent Poet' ),
-    	'parent_item_colon' => __( 'Parent Poet:' ),
-    	'edit_item' 		=> __( 'Edit Poet' ), 
-    	'update_item' 		=> __( 'Update Poet' ),
-    	'add_new_item' 		=> __( 'Add New Poet' ),
-    	'new_item_name' 	=> __( 'New Poet Name' ),
-    	'menu_name' 		=> __( 'Poet' ),
+		'name' 							=> _x( 'Poets', 'taxonomy general name' ),
+    'singular_name' 		=> _x( 'Poet', 'taxonomy singular name' ),
+    'search_items' 			=> __( 'Search Poets' ),
+    'all_items' 				=> __( 'All Poets' ),
+    'parent_item' 			=> __( 'Parent Poet' ),
+    'parent_item_colon' => __( 'Parent Poet:' ),
+    'edit_item' 				=> __( 'Edit Poet' ), 
+    'update_item' 			=> __( 'Update Poet' ),
+    'add_new_item' 			=> __( 'Add New Poet' ),
+    'new_item_name' 		=> __( 'New Poet Name' ),
+    'menu_name' 				=> __( 'Poet' ),
 	);
 
 	$args = array(
-		'hierarchical' 		=> true,
-    	'labels' 			=> $labels,
-    	'show_ui' 			=> true,
-    	'show_in_rest' 		=> true,
-    	'show_admin_column' => true,
-    	'query_var' 		=> true,
-    	'rewrite'			=> [ 'slug' => 'poet' ]
+		'hierarchical' 			=> true,
+    'labels' 						=> $labels,
+    'show_ui' 					=> true,
+    'show_in_rest' 			=> true,
+    'show_admin_column' => true,
+    'query_var' 				=> true,
+    'rewrite'						=> [ 'slug' => 'poet' ]
 	);
 
 	register_taxonomy('poet', [ 'poem' ], $args);
@@ -79,27 +79,27 @@ function lone_create_poet_taxonomy() {
 function lone_create_artist_taxonomy() {
 
 	$labels = array(
-		'name' 				=> _x( 'Artists', 'taxonomy general name' ),
-    	'singular_name' 	=> _x( 'Artist', 'taxonomy singular name' ),
-    	'search_items' 		=> __( 'Search Artists' ),
-    	'all_items' 		=> __( 'All Artists' ),
-    	'parent_item' 		=> __( 'Parent Artist' ),
-    	'parent_item_colon' => __( 'Parent Artist:' ),
-    	'edit_item' 		=> __( 'Edit Artist' ), 
-    	'update_item' 		=> __( 'Update Artist' ),
-    	'add_new_item' 		=> __( 'Add New Artist' ),
-    	'new_item_name' 	=> __( 'New Artist Name' ),
-    	'menu_name' 		=> __( 'Artist' ),
+		'name' 							=> _x( 'Artists', 'taxonomy general name' ),
+    'singular_name' 		=> _x( 'Artist', 'taxonomy singular name' ),
+    'search_items' 			=> __( 'Search Artists' ),
+    'all_items' 				=> __( 'All Artists' ),
+    'parent_item' 			=> __( 'Parent Artist' ),
+    'parent_item_colon' => __( 'Parent Artist:' ),
+    'edit_item' 				=> __( 'Edit Artist' ), 
+    'update_item' 			=> __( 'Update Artist' ),
+    'add_new_item' 			=> __( 'Add New Artist' ),
+    'new_item_name' 		=> __( 'New Artist Name' ),
+    'menu_name' 				=> __( 'Artist' ),
 	);
 
 	$args = array(
-		'hierarchical' 		=> true,
-    	'labels' 			=> $labels,
-    	'show_ui' 			=> true,
-    	'show_in_rest' 		=> true,
-    	'show_admin_column' => true,
-    	'query_var' 		=> true,
-    	'rewrite'			=> [ 'slug' => 'artist' ]
+		'hierarchical' 			=> true,
+    'labels' 						=> $labels,
+    'show_ui' 					=> true,
+    'show_in_rest' 			=> true,
+    'show_admin_column' => true,
+    'query_var' 				=> true,
+    'rewrite'						=> [ 'slug' => 'artist' ]
 	);
 
 	register_taxonomy('artist', [ 'art_piece' ], $args);
@@ -114,43 +114,43 @@ add_action( 'init', 'lone_create_artist_taxonomy', 0);
 
 function lone_create_poem_type() {
 	$poem_labels = array(
-		'name' => _X('Poems', 'Post Type General Name', 'lone'),
-		'singular_name' => _X('Poem', 'Post Type Singular Name', 'lone'),
-		'menu_name' => __('Poems', 'lone'),
-		'parent_item_colon' => __('Parent Poem:', 'lone'),
-		'all_items' => __('All Poems', 'lone'),
-		'view_item' => __('View Poem', 'lone'),
-		'add_new_item' => __('Add Poem', 'lone'),
-		'add_new' => __('Add New', 'lone'),
-		'edit_item' => __('Edit Poem', 'lone'),
-		'update_item' => __('Update Poem', 'lone'),
-		'search_items' => __('Search Poems', 'lone'),
-		'not_found' => __('Not Found', 'lone'),
-		'not_found_in_trash' => __('Not found in Trash', 'lone')
+		'name' 								=> _X('Poems', 'Post Type General Name', 'lone'),
+		'singular_name' 			=> _X('Poem', 'Post Type Singular Name', 'lone'),
+		'menu_name' 					=> __('Poems', 'lone'),
+		'parent_item_colon' 	=> __('Parent Poem:', 'lone'),
+		'all_items' 					=> __('All Poems', 'lone'),
+		'view_item' 					=> __('View Poem', 'lone'),
+		'add_new_item' 				=> __('Add Poem', 'lone'),
+		'add_new' 						=> __('Add New', 'lone'),
+		'edit_item' 					=> __('Edit Poem', 'lone'),
+		'update_item' 				=> __('Update Poem', 'lone'),
+		'search_items' 				=> __('Search Poems', 'lone'),
+		'not_found' 					=> __('Not Found', 'lone'),
+		'not_found_in_trash'	=> __('Not found in Trash', 'lone')
 	);
 
 	$poem_args = array(
-		'label' => __('poems', 'lone'),
-		'description' => __('Poems', 'lone'),
-		'labels' => $poem_labels,
+		'label' 							=> __('poems', 'lone'),
+		'description' 				=> __('Poems', 'lone'),
+		'labels' 							=> $poem_labels,
 
 		//Post Editor features
-		'supports' => array('title', 'editor', 'excerpt', 'author', 'custom-fields'),
+		'supports' 						=> array('title', 'editor', 'excerpt', 'author', 'custom-fields'),
 
 		//associating with taxonomy
-		'taxonomies' => array('poet'),
+		'taxonomies' 					=> array('poet'),
 
-		'heirarchical' => true,
-		'public' => true,
-		'rewrite' => array('slug' => 'poems'),
-		'show_ui' => true,
-		'show_in_menu' => true,
-		'can_export' => true,
-		'has_archive' => true,
+		'heirarchical' 				=> true,
+		'public' 							=> true,
+		'rewrite' 						=> array('slug' => 'poems'),
+		'show_ui' 						=> true,
+		'show_in_menu' 				=> true,
+		'can_export' 					=> true,
+		'has_archive' 				=> true,
 		'exclude_from_search' => false,
-		'publicly_queryable' => true,
-		'capability_type' => 'post',
-		'show_in_rest' => true
+		'publicly_queryable' 	=> true,
+		'capability_type' 		=> 'post',
+		'show_in_rest' 				=> true
 	);
 
 	register_post_type( 'poems', $poem_args);
@@ -159,43 +159,43 @@ function lone_create_poem_type() {
 
 function lone_create_art_piece_type() {
 	$art_labels = array(
-		'name' => _X('Art', 'Post Type General Name', 'lone'),
-		'singular_name' => _X('Piece', 'Post Type Singular Name', 'lone'),
-		'menu_name' => __('Art', 'lone'),
-		'parent_item_colon' => __('Art:', 'lone'),
-		'all_items' => __('All Art', 'lone'),
-		'view_item' => __('View Piece', 'lone'),
-		'add_new_item' => __('Add Piece', 'lone'),
-		'add_new' => __('Add New', 'lone'),
-		'edit_item' => __('Edit Piece', 'lone'),
-		'update_item' => __('Update Piece', 'lone'),
-		'search_items' => __('Search Art', 'lone'),
-		'not_found' => __('Not Found', 'lone'),
-		'not_found_in_trash' => __('Not found in Trash', 'lone')
+		'name' 								=> _X('Art', 'Post Type General Name', 'lone'),
+		'singular_name' 			=> _X('Piece', 'Post Type Singular Name', 'lone'),
+		'menu_name' 					=> __('Art', 'lone'),
+		'parent_item_colon' 	=> __('Art:', 'lone'),
+		'all_items' 					=> __('All Art', 'lone'),
+		'view_item' 					=> __('View Piece', 'lone'),
+		'add_new_item' 				=> __('Add Piece', 'lone'),
+		'add_new' 						=> __('Add New', 'lone'),
+		'edit_item' 					=> __('Edit Piece', 'lone'),
+		'update_item'					=> __('Update Piece', 'lone'),
+		'search_items' 				=> __('Search Art', 'lone'),
+		'not_found' 					=> __('Not Found', 'lone'),
+		'not_found_in_trash' 	=> __('Not found in Trash', 'lone')
 	);
 
 	$art_args = array(
-		'label' => __('art', 'lone'),
-		'description' => __('Art Pieces', 'lone'),
-		'labels' => $art_labels,
+		'label' 							=> __('art', 'lone'),
+		'description' 				=> __('Art Pieces', 'lone'),
+		'labels' 							=> $art_labels,
 
 		//Post Editor features
-		'supports' => array('title', 'thumbnail', 'custom-fields'),
+		'supports' 						=> array('title', 'thumbnail', 'custom-fields'),
 
 		//associating with taxonomy
-		'taxonomies' => array('artist'),
+		'taxonomies' 					=> array('artist'),
 
-		'heirarchical' => true,
-		'public' => true,
-		'rewrite' => array('slug' => 'art'),
-		'show_ui' => true,
-		'show_in_menu' => true,
-		'can_export' => true,
-		'has_archive' => true,
+		'heirarchical' 				=> true,
+		'public' 							=> true,
+		'rewrite' 						=> array('slug' => 'art'),
+		'show_ui' 						=> true,
+		'show_in_menu' 				=> true,
+		'can_export' 					=> true,
+		'has_archive' 				=> true,
 		'exclude_from_search' => false,
-		'publicly_queryable' => true,
-		'capability_type' => 'post',
-		'show_in_rest' => true
+		'publicly_queryable' 	=> true,
+		'capability_type' 		=> 'post',
+		'show_in_rest' 				=> true
 	);
 
 	register_post_type( 'art', $art_args);
